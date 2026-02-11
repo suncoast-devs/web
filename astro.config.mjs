ff8c844acc9f96629b171e26552c70ad200d0504
Jason L Perry <298+ambethia@users.noreply.github.com>
2026-02-10 23:06:12 -0500

    Fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: node({
-		mode: "standalone",
-	}),
-	server: {
-		port: 4321,
-		host: true,
-	},
 });
